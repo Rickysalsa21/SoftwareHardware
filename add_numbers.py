9ca28baa0789b424011bbfba91eef1304802188a
Rickysalsa21 <99295783+Rickysalsa21@users.noreply.github.com>
2023-10-18 11:41:57 -0400

    third commit

---
+++ add_numbers.py
@@ -0,0 +1,11 @@
+def main():
+
+
+    print("This program adds two nums")
+
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number"))
+    total = num1 + num2
+    print("hello" +total)
+if __name__ == "__main__":
+    main()
---
+++ first_pything.py
@@ -0,0 +1,11 @@
+def main():
+    num1 = 125
+    num2 = 100
+    num3 = num1 - num2
+    print(num3)
+    num3 = 10
+    num1 = 20
+    num2 = num1 + num2 + num3
+    print(num3 + num2)
+if __name__ == "__main__":
+    main()
