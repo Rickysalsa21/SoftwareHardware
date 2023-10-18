23f299667aea336d6d18f1654206c329bcfe3b11
Rickysalsa21 <99295783+Rickysalsa21@users.noreply.github.com>
2023-10-18 11:38:27 -0400

    second commit

---
+++ add_numbers-1.py
@@ -0,0 +1,11 @@
+def main():
+
+    print("This program adds to nums")
+
+    num1 = input("Enter the first number: ")
+    num2 = input("Enter the second number")
+    total = int(num1) +  int(num2)
+   
+    print("The total is " + str(total)+".")
+if __name__ == "__main__":
+    main()
